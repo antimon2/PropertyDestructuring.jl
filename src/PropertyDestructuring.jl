36a6581f7fd3f3eca0df3f426c9f77b6a35f1ef7
GOTOH Shunsuke <antimon2.me@gmail.com>
2021-11-03 13:19:18 +0900

    1st commit

--- src/PropertyDestructuring.jl
+++ src/PropertyDestructuring.jl
@@ -1,5 +1,184 @@
 module PropertyDestructuring
 
-# Write your package code here.
+using ExprTools
+
+export @destructure
+
+function _is_property_assignee(ex)
+    Meta.isexpr(ex, :tuple) && (Meta.isexpr(ex.args[1], :parameters) || any(_is_property_assignee, ex.args))
+end
+
+function _is_destructure_property_assignment(ex)
+    Meta.isexpr(ex, :(=)) && _is_property_assignee(ex.args[1])
+end
+
+function _is_destructure_property_for(ex)
+    Meta.isexpr(ex, :for) && (
+        _is_destructure_property_assignment(ex.args[1]) ||
+        Meta.isexpr(ex.args[1], :block) && any(_is_destructure_property_assignment, ex.args[1].args)
+    )
+end
+
+function _destructure_property_for_multi(ex)
+    blk = _destructure(ex.args[2])
+    variables = Dict{Symbol, Vector{Any}}()
+    in_clauses = Expr(:block)
+    for assignment_src in ex.args[1].args
+        new_assignee = _deconstruct_assignees!(assignment_src.args[1], variables)
+        push!(in_clauses.args, :($new_assignee = $(assignment_src.args[2])))
+    end
+    assignments = Expr(:block, (
+        :($var = getproperty($sym, $(QuoteNode(var)))) for sym in keys(variables) for var in variables[sym]
+    )...)
+    Expr(:for, in_clauses, quote
+        $assignments
+        $blk
+    end)
+end
+
+function _destructure_property_for_complex(ex)
+    blk = _destructure(ex.args[2])
+    new_assignee, variables = _deconstruct_assignees(ex.args[1].args[1])
+    src = _destructure(ex.args[1].args[2])
+    assignments = Expr(:block, (
+        :($var = getproperty($sym, $(QuoteNode(var)))) for sym in keys(variables) for var in variables[sym]
+    )...)
+    :(for $new_assignee in $src
+        $assignments
+        $blk
+    end)
+end
+
+function _destructure_property_for(ex)
+    # @assert Meta.isexpr(ex, :for) && _is_destructure_property_assignment(ex.args[1])
+    Meta.isexpr(ex.args[1], :block) && return _destructure_property_for_multi(ex)
+    Meta.isexpr(ex.args[1].args[1], :tuple) && Meta.isexpr(ex.args[1].args[1].args[1], :parameters) || 
+        return _destructure_property_for_complex(ex)
+    blk = _destructure(ex.args[2])
+    variables = ex.args[1].args[1].args[1].args
+    lh = Expr(:tuple, variables...)
+    _el = gensym(:el)
+    gvars = Expr(:tuple, (:($_el.$var) for var in variables)...)
+    src0 = _destructure(ex.args[1].args[2])
+    src = :(($gvars for $_el in $(src0)))
+    Expr(:for, :($lh = $src), blk)
+end
+
+_deconstruct_assignees!(ex, _variables) = ex
+function _deconstruct_assignees!(ex::Expr, variables)
+    if Meta.isexpr(ex, :tuple) && Meta.isexpr(ex.args[1], :parameters)
+        new_symbol = gensym(string(ex))
+        variables[new_symbol] = ex.args[1].args
+        return new_symbol
+    end
+    Expr(ex.head, (_deconstruct_assignees!(arg, variables) for arg in ex.args)...)
+end
+
+function _deconstruct_assignees(ex)
+    variables = Dict{Symbol, Vector{Any}}()
+    new_ex = _deconstruct_assignees!(ex, variables)
+    (new_ex, variables)
+end
+
+function _destructure_property_assignment_complex(ex)
+    # @assert _is_destructure_property_assignment(ex)
+    new_assignee, variables = _deconstruct_assignees(ex.args[1])
+    src = _destructure(ex.args[2])
+    assignments = Expr(:block, (
+        :($var = getproperty($sym, $(QuoteNode(var)))) for sym in keys(variables) for var in variables[sym]
+    )...)
+    quote
+        $new_assignee = $src
+        $assignments
+        $src
+    end
+end
+
+function _destructure_property_assignment(ex)
+    # @assert _is_destructure_property_assignment(ex)
+    Meta.isexpr(ex.args[1], :tuple) && Meta.isexpr(ex.args[1].args[1], :parameters) || 
+        return _destructure_property_assignment_complex(ex)
+    variables = ex.args[1].args[1].args
+    src = _destructure(ex.args[2])
+    assignments = Expr(:block, (:($(var) = getproperty($src, $(QuoteNode(var)))) for var in variables)...)
+    quote
+        $assignments
+        $src
+    end
+end
+
+# original(inspired by): https://github.com/FluxML/MacroTools.jl/blob/65c55530b63918daac5f041b144c50d4e34e7984/src/utils.jl#L438-L452
+splitarg(name::Symbol) = (name, :Any, false, nothing)
+function splitarg(arg_expr)
+    if Meta.isexpr(arg_expr, :kw)
+        # デフォルト値が設定されている場合
+        (splitarg(arg_expr.args[1])[1:3]..., arg_expr.args[2])
+    elseif Meta.isexpr(arg_expr, :(...))
+        # `...` が指定されている場合
+        (splitarg(arg_expr.args[1])[1:2]..., true, nothing)
+    elseif Meta.isexpr(arg_expr, :(::))
+        # 型アノテーションが存在する場合
+        _name = length(arg_expr.args) > 1 ? arg_expr.args[1] : nothing
+        _type = arg_expr.args[end]
+        (_name, _type, false, nothing)
+    else
+        (arg_expr, :Any, false, nothing)
+    end
+end
+
+# original(inspired by): https://github.com/FluxML/MacroTools.jl/blob/65c55530b63918daac5f041b144c50d4e34e7984/src/utils.jl#L414-L418
+function combinearg(_name, _type, _isslurp, _default)
+    ex = isnothing(_name) ? :(::$_type) : :($_name::$_type)
+    if _isslurp
+        ex = Expr(:(...), ex)
+    end
+    if !isnothing(_default)
+        ex = Expr(:kw, ex, _default)
+    end
+    ex
+end
+
+function _destructure_function_definition(ex)
+    # @assert ExprTools.isdef(ex)
+    def_dict = ExprTools.splitdef(ex)
+    body = def_dict[:body]
+    # args = ExprTools.splitarg.(def_dict[:args])
+    # if !any(_is_property_assignee, (arg[1] for arg in args))
+    assignments = Expr[]
+    if haskey(def_dict, :args)
+        for (i, arg) in enumerate(def_dict[:args])
+            (_name, _type, _slurp, _default) = splitarg(arg)
+            if _is_property_assignee(_name)
+                newvar = gensym()
+                def_dict[:args][i] = combinearg(newvar, _type, _slurp, _default)
+                variables = _name.args[1].args
+                append!(assignments, [:($(var) = getproperty($newvar, $(QuoteNode(var)))) for var in variables])
+            end
+        end
+    end
+    body = Expr(body.head, _destructure.(body.args)...)
+    if !isempty(assignments)
+        body = Expr(:block, assignments..., body)
+    end
+    def_dict[:body] = body
+    ExprTools.combinedef(def_dict)
+end
+
+isdef(ex) = false
+isdef(ex::Expr) = !isnothing(ExprTools.splitdef(ex; throw=false))
+
+_destructure(ex) = ex
+
+function _destructure(ex::Expr)
+    _is_destructure_property_for(ex) && return _destructure_property_for(ex)
+    _is_destructure_property_assignment(ex) && return _destructure_property_assignment(ex)
+    isdef(ex) && return _destructure_function_definition(ex)
+    Expr(ex.head, _destructure.(ex.args)...)
+end
+
+macro destructure(ex::Expr)
+    VERSION < v"1.7.0-DEV.364" || return esc(ex)
+    esc(_destructure(ex))
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,141 @@
+module PropertyDestructuringTest
+
 using PropertyDestructuring
 using Test
 
-@testset "PropertyDestructuring.jl" begin
-    # Write your tests here.
+struct Point2D{T}
+    x::T
+    y::T
 end
+
+struct Point3D{T}
+    x::T
+    y::T
+    z::T
+end
+
+@testset "PropertyDestructuring.jl" begin
+
+    @testset "Assignment" begin
+        pt1 = Point2D(1, 2)
+        @destructure (; x, y) = pt1
+        @test x == 1
+        @test y == 2
+
+        pt2 = Point2D(3.14, 9.99)
+        @destructure (; x, y) = pt2
+        @test x == 3.14
+        @test y == 9.99
+
+        pt3 = Point3D(1.2, 3.4, 5.6)
+        @destructure (; x, y) = pt3
+        @test x == 1.2
+        @test y == 3.4
+
+        @destructure (; x, y) = (x=:x, y="歪")
+        @test x === :x
+        @test y == "歪"
+    end
+
+    @testset "Complex Assignment" begin
+        pt1 = Point2D(1, 2)
+        @destructure (_DUMMY, (; x, y)) = (:DUMMY, pt1)
+        @test x == 1
+        @test y == 2
+
+        pt2 = Point2D(3.14, 9.99)
+        @destructure ((; x, y), _DUMMY) = (pt2, :DUMMY)
+        @test x == 3.14
+        @test y == 9.99
+
+        pt3 = Point3D(1.2, 3.4, 5.6)
+        @destructure ((; x, y), ((; z, w), _DUMMY)) = (pt3, ((z=:z, w="W"), :DUMMY))
+        @test x == 1.2
+        @test y == 3.4
+        @test z === :z
+        @test w == "W"
+    end
+
+    @testset "Function Argument (1)" begin
+        @destructure function _test1((; x, y))
+            (x, y)
+        end
+
+        pt1 = Point2D(1, 2)
+        x, y = _test1(pt1)
+        @test x == 1
+        @test y == 2
+
+        pt2 = Point2D(3.14, 9.99)
+        x, y = _test1(pt2)
+        @test x == 3.14
+        @test y == 9.99
+
+        pt3 = Point3D(1.2, 3.4, 5.6)
+        x, y = _test1(pt3)
+        @test x == 1.2
+        @test y == 3.4
+
+        x, y = _test1((x=:x, y="歪"))
+        @test x === :x
+        @test y == "歪"
+    end
+
+    @testset "Function Argument (2)" begin
+        @destructure _test2((; x, y)) = (x, y)
+
+        pt1 = Point2D(1, 2)
+        x, y = _test2(pt1)
+        @test x == 1
+        @test y == 2
+
+        pt2 = Point2D(3.14, 9.99)
+        x, y = _test2(pt2)
+        @test x == 3.14
+        @test y == 9.99
+
+        pt3 = Point3D(1.2, 3.4, 5.6)
+        x, y = _test2(pt3)
+        @test x == 1.2
+        @test y == 3.4
+
+        x, y = _test2((x=:x, y="歪"))
+        @test x === :x
+        @test y == "歪"
+    end
+
+    @testset "`in` clause of `for`" begin
+        pts = [
+            Point2D(1, 2),
+            Point2D(3.14, 9.99),
+            Point3D(1.2, 3.4, 5.6),
+            (x=:x, y="歪"),
+        ]
+
+        index = 1
+        @destructure for (; x, y) in pts
+            @test x == pts[index].x
+            @test y == pts[index].y
+            index += 1
+        end
+
+        @destructure for (pt, (; x, y)) in zip(pts, pts)
+            @test x == pt.x
+            @test y == pt.y
+        end
+
+        index = 1
+        @destructure for (; x, y) in pts, _DUMMY in 1
+            @test x == pts[index].x
+            @test y == pts[index].y
+            index += 1
+        end
+
+        @destructure for (pt, (; x, y)) in zip(pts, pts), _DUMMY in 1
+            @test x == pt.x
+            @test y == pt.y
+        end
+    end
+end
+
+end  # module
